157cc1228f2b908ccc1ea083fd7e03f2410a30da
aisa-droid <ayeejay.ij@gmail.com>
2026-03-12 01:00:23 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import KoraPart1 from "src/components/KORA-v8.1.1-part1.jsx"
-import KoraPart2 from "src/components/KORA-v8.1.1-part1.jsx"
+import KoraPart1 from "./components/KORA-v8.1.1-part1.jsx"
+import KoraPart2 from "./components/KORA-v8.1.1-part1.jsx"
 
 export default function App() {
   return (
