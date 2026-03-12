7a09a7771e9ce8b1d2cb43bd0ad83ee622943787
aisa-droid <ayeejay.ij@gmail.com>
2026-03-11 23:28:33 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import KoraPart1 from "../KORA-v8.1.1-part1.jsx"
+import KoraPart2 from "../KORA-v8.1.1-part2.jsx"
+
+export default function App() {
+  return (
+    <>
+      <KoraPart1 />
+      <KoraPart2 />
+    </>
+  )
+}
